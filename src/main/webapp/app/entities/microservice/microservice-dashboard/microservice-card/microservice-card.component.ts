fdecd0b7fb1d94aa32a9c3a46d835ab55e12356a
tillias <ternovich@gmail.com>
2020-10-04 13:02:48 +0200

    Introduced CardSettings for more flexible configuration #13

--- src/main/webapp/app/entities/microservice/microservice-dashboard/microservice-card/microservice-card.component.ts
+++ src/main/webapp/app/entities/microservice/microservice-dashboard/microservice-card/microservice-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { IMicroservice } from 'app/shared/model/microservice.model';
+import { CardSettings, ICardSettings } from 'app/entities/microservice/microservice-dashboard/microservice-card/card-settings.model';
 
 @Component({
   selector: 'jhi-microservice-card',
@@ -8,8 +9,17 @@ import { IMicroservice } from 'app/shared/model/microservice.model';
 })
 export class MicroserviceCardComponent implements OnInit {
   @Input() microservice!: IMicroservice;
+  @Input() settings: ICardSettings = CardSettings.DEFAULT;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.assertInputsProvided();
+  }
+
+  private assertInputsProvided(): void {
+    if (!this.microservice || !this.settings) {
+      throw new Error('The required inputs [microservice] or [cardSettings] are not provided');
+    }
+  }
 }
