3e2a6a53f81f50f3d6db1026a683862a808e5bd7
tillias <ternovich@gmail.com>
2020-10-05 18:49:03 +0200

    Added vis-network sample #2
    Tracking https://github.com/visjs/vis-network/issues/1085

--- src/main/webapp/app/dashboard/dependency-dashboard/dependency-dashboard.component.ts
+++ src/main/webapp/app/dashboard/dependency-dashboard/dependency-dashboard.component.ts
@@ -1,12 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { DataSet } from 'vis-data';
+import { Network } from 'vis-network';
 
 @Component({
   selector: 'jhi-dependency-dashboard',
   templateUrl: './dependency-dashboard.component.html',
   styleUrls: ['./dependency-dashboard.component.scss'],
 })
-export class DependencyDashboardComponent implements OnInit {
+export class DependencyDashboardComponent implements OnInit, AfterViewInit {
+  @ViewChild('visNetwork', { static: false }) visNetwork!: ElementRef;
+  private networkInstance: any;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    // create an array with nodes
+    const nodes = new DataSet<any>([
+      { id: 1, label: 'Node 1' },
+      { id: 2, label: 'Node 2' },
+      { id: 3, label: 'Node 3' },
+      { id: 4, label: 'Node 4' },
+      { id: 5, label: 'Node 5' },
+    ]);
+
+    // create an array with edges
+    const edges = new DataSet<any>([
+      { from: '1', to: '3' },
+      { from: '1', to: '2' },
+      { from: '2', to: '4' },
+      { from: '2', to: '5' },
+    ]);
+
+    const data = { nodes, edges };
+
+    const container = this.visNetwork;
+    this.networkInstance = new Network(container.nativeElement, data, {});
+  }
 }
