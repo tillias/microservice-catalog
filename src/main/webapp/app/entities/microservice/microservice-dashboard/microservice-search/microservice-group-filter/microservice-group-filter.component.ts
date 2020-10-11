5fc5b16595ae8c9debd552b7e70f95d8f774c72e
tillias <ternovich@gmail.com>
2020-10-11 09:44:02 +0200

    Status and Team can now be used as a filter #16

--- src/main/webapp/app/entities/microservice/microservice-dashboard/microservice-search/microservice-group-filter/microservice-group-filter.component.ts
+++ src/main/webapp/app/entities/microservice/microservice-dashboard/microservice-search/microservice-group-filter/microservice-group-filter.component.ts
@@ -1,5 +1,10 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { MicroserviceGroupFilter, IMicroserviceGroupFilter } from 'app/shared/model/util/microservice-group-filter';
+import { IMicroserviceGroupFilter, MicroserviceGroupFilter } from 'app/shared/model/util/microservice-group-filter';
+import { TeamService } from 'app/entities/team/team.service';
+import { StatusService } from 'app/entities/status/status.service';
+import { ITeam } from 'app/shared/model/team.model';
+import { IStatus } from 'app/shared/model/status.model';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'jhi-microservice-group-filter',
@@ -8,14 +13,25 @@ import { MicroserviceGroupFilter, IMicroserviceGroupFilter } from 'app/shared/mo
 })
 export class MicroserviceGroupFilterComponent implements OnInit {
   groupFilter: IMicroserviceGroupFilter;
+  teams: ITeam[];
+  statuses: IStatus[];
 
   @Output() filterChanged = new EventEmitter<IMicroserviceGroupFilter>();
 
-  constructor() {
+  constructor(private teamService: TeamService, private statusService: StatusService) {
     this.groupFilter = new MicroserviceGroupFilter();
+    this.teams = [];
+    this.statuses = [];
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.loadItems();
+  }
+
+  loadItems(): void {
+    this.teamService.query().subscribe((res: HttpResponse<ITeam[]>) => (this.teams = res.body || []));
+    this.statusService.query().subscribe((res: HttpResponse<IStatus[]>) => (this.statuses = res.body || []));
+  }
 
   onSearch(): void {
     this.filterChanged.emit(this.groupFilter);
