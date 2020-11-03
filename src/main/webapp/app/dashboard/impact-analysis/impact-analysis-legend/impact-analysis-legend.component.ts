5ecec96c527a2c89642c20b2b2cff16a9ebe746a
tillias <ternovich@gmail.com>
2020-11-03 19:41:03 +0100

    Added impact analysis graph and legend ui components #79

--- src/main/webapp/app/dashboard/impact-analysis/impact-analysis-legend/impact-analysis-legend.component.ts
+++ src/main/webapp/app/dashboard/impact-analysis/impact-analysis-legend/impact-analysis-legend.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IResult } from '../../../shared/model/impact/analysis/result.model';
 import { ActivatedRoute } from '@angular/router';
+import { IMicroservice } from '../../../shared/model/microservice.model';
 
 @Component({
   selector: 'jhi-impact-analysis-legend',
@@ -9,12 +10,22 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ImpactAnalysisLegendComponent implements OnInit {
   analysisResult?: IResult;
+  dependencies: IMicroservice[] = [];
+  page = 1;
+  pageSize = 15;
 
   constructor(protected activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ analysisResult }) => {
       this.analysisResult = analysisResult;
+
+      this.initDependencies();
     });
+  }
+
+  initDependencies(): void {
+    this.dependencies = [];
+    this.analysisResult?.groups?.forEach(v => v.items?.forEach(i => this.dependencies.push(i.target)));
   }
 }
